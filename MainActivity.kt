3e5a7122033cc3826622a0319f1025f7f3642d13
Ghadi Ayoub <121671297+Ghadi03@users.noreply.github.com>
2023-05-28 23:29:25 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,21 +3,69 @@ import android.content.Intent
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import com.example.areeba.R.*
+import com.example.areeba.R.id.drawer_layout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            // Create an intent to open the new activity
-            val intent = Intent(this, MainPage::class.java)
-            startActivity(intent)
+        setContentView(R.layout.main_activity)
 
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
 
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, homepage()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, homepage()).commit()
+            R.id.arabic -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Arabic()).commit()
+            R.id.eng -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, english()).commit()
+            R.id.album -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, album()).commit()
+            R.id.nav_contact -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, contact()).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
 }
